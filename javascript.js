e34947bbc05f1d66183c7c77cd1931ccadd2d20c
gabrielpoll <gabrieldarosa.poll@gmail.com>
2025-08-29 20:20:33 -0300

    Fix row width and height in JS file

--- javascript.js
+++ javascript.js
@@ -1,15 +1,20 @@
 getQuantityButton = document.querySelector(".quantity-button");
 getSketchContainer = document.querySelector(".sketch-container");
-getSketchScreen = document.querySelector(".sketch-screen");
+
 
 function makeGrids(size) {
+    const createSketchScreen = document.createElement("div");
+    createSketchScreen.classList.add("sketch-screen");
+    getSketchContainer.appendChild(createSketchScreen);
     for (let i = 0; i < size; i++) {
         let column = document.createElement("div");
         column.classList.add("column");
         for (let j = 1; j <= size; j++) {
             let row = document.createElement("div");
             row.classList.add("row");
-            row.style.border = "2px solid black";
+            row.style.border = "1px solid black";
+            row.style.width = `${600 / size}px`;
+            row.style.height = `${600 / size}px`;
 
             row.addEventListener("mouseover", function(e) {
                 row.style.backgroundColor = "blue";
@@ -17,15 +22,20 @@ function makeGrids(size) {
 
             column.appendChild(row);
         }
-        getSketchScreen.appendChild(column)
+        createSketchScreen.appendChild(column)
     }
 }
 
+makeGrids(16);
+
 getQuantityButton.addEventListener("click", function(e) {
-    let getQuantitySquares = prompt("How many squares do you want?");
-    while (getQuantitySquares > 100) {
+    let getQuantitySquares = parseInt(prompt("How many squares do you want?"));
+    
+    while (getQuantitySquares > 100 || isNaN(getQuantitySquares)) {
         alert("No more than 100!");
-        getQuantitySquares = prompt("How many squares do you want?");
+        getQuantitySquares = parseInt(prompt("How many squares do you want?"));
     };
+
+    getSketchContainer.innerHTML = "";
     makeGrids(getQuantitySquares);
 });
